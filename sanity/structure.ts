7bd97b8aba7030eee204210120c16de92285c1d1
Rafael Rodrigues <rafa88.dev@gmail.com>
2026-02-19 09:48:32 -0300

    bug fix structure and config sanity

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,5 @@
-import type {StructureResolver} from 'sanity/structure'
-
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure = (S: any) =>
   S.list()
     .title('Blog')
     .items([
@@ -9,7 +7,8 @@ export const structure: StructureResolver = (S) =>
       S.documentTypeListItem('category').title('Categories'),
       S.documentTypeListItem('author').title('Authors'),
       S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
-      ),
+      ...S.documentTypeListItems().filter((item: any) => {
+        const id = item.getId && item.getId()
+        return id && !['post', 'category', 'author'].includes(id)
+      }),
     ])
