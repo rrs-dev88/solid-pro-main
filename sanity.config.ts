0f6ca8c2eb9d0b5d81a83a682e4fc8515529ace1
Rafael Rodrigues <rafa88.dev@gmail.com>
2026-02-19 08:35:00 -0300

    include sanity, and create structure blog

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,28 @@
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import schemas from "./sanity/schemas";
+'use client'
 
-const config = defineConfig({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  title: process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE as string,
-  dataset: "production",
-  apiVersion: "2023-06-18",
-  basePath: "/admin",
-  plugins: [structureTool()],
-  schema: { types: schemas },
-});
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ */
 
-export default config;
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schemaTypes'
+import {structure} from './sanity/structure'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
+  plugins: [
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
